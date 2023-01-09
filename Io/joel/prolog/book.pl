e90d727537aa5d941bd7cb5d3246055b6be8b27a
khcho07 <75979031+khcho07@users.noreply.github.com>
2023-01-09 22:26:22 +0900

    Add files via upload
    prolog day 1 homework

---
+++ Io/joel/prolog/book.pl
@@ -0,0 +1,9 @@
+wrote(george_rr_martin, a_game_of_thrones).
+wrote(george_rr_martin, a_clash_of_kings).
+wrote(cormac_mccarthy, the_road).
+wrote(edwin_abbott, flatland).
+wrote(aurthur_conan_doyle, the_adventures_of_sherlock_holmes).
+wrote(ken_kesey, one_flew_over_the_cuckoos_nest).
+wrote(douglas_adams, the_hitchhikers_guide_to_the_galaxy).
+wrote(douglas_adams, the_restaurant_at_the_end_of_the_universe).
+wrote(douglas_adams, life_the_universe_and_everything).
